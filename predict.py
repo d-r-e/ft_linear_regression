6024324016ef81b526be9e15bd8863e2c30ab5ba
David R <egoz3ntrum@gmail.com>
2020-10-22 19:39:24 +0200

    ft_linear_regression by darodrig

--- predict.py
+++ predict.py
@@ -1,8 +1,18 @@
-#! /usr/bin/env python3 
+#! /usr/bin/env python3
 import numpy as np
 
 if __name__ == "__main__":
-	theta = np.zeros(2)
-	mileage = float(input("Please enter mileage: "))
-	price = theta[0] + theta[1] * mileage
-	print(price)
+    try:
+        filename = "theta.txt"
+        f = open(filename, "r")
+        theta = np.zeros(2)
+        theta[0] = float(f.readline())
+        theta[1] = float(f.readline())
+        f.close()
+    except:
+        print(f"Error: \"{filename}\" file not found or wrong format.")
+        exit(0)
+        
+    mileage = float(input("Please enter mileage: "))
+    price = theta[0] + theta[1] * mileage
+    print(f'Predicted price: {price}')
