c15a838b1773893dbf6004f87792b0130e863d15
David Rodriguez <darodrig@c3r9s1.42madrid.com>
2020-10-29 12:37:10 +0100

    ft_linear_regression by darodrig

--- train.py
+++ train.py
@@ -13,8 +13,8 @@
 import numpy as np
 import pandas as pd
 
-def estimate_price(theta, mileage):
-    price = theta[0] + theta[1] * mileage
+def estimate_price(mileage, theta0, theta1):
+    price = theta0 + (theta1 * mileage)
     return price
 
 def predict(theta, x):
@@ -23,34 +23,21 @@ def predict(theta, x):
     y = xx.dot(theta)
     return y
 
-def fit(theta, X, Y, learning_rate, n_cycle):
-    price = X
-    if type(theta) != np.ndarray or theta.size == 0:
-        print("mismatching types")
-        print(type(theta) + " != np.ndarray")
-        return None
-    if type(X) != np.ndarray or X.size == 0:
-        print(type(X))
-        print("Incorrect X array")
-        return None
-    if type(Y) != np.ndarray or Y.size == 0:
-        print("Incorrect Y array")
-        return None
-    ones = np.ones((X.shape[0], 2))
-    ones[:,-1] = X
-    m = theta.size
-    
-    theta0 = theta[0]
-    theta1 = theta[1]
-    for i in range(0, n_cycle):
-        hypothesis = estimate_price(theta, X)
-        tmp_theta0 = learning_rate * 1/m * sum(hypothesis - price)
-        tmp_theta1 = learning_rate * 1/m * sum((hypothesis - price) * X)
+def linear_regression(mileage, price, m, theta0, theta1, learning_rate, ntimes):
+    m = float(m)
+    x = mileage
+    y = price
+    mileage = (mileage-min(mileage))/(max(mileage)-min(mileage))
+    price = (price-min(price))/(max(price)-min(price))
+    for i in range(ntimes):
+        yp = estimate_price(mileage, theta0, theta1)
+        tmp_theta0 = learning_rate * 1/m * sum(yp - price)
+        tmp_theta1 = learning_rate * 1/m * sum((yp - price) * mileage)
         theta0 -= tmp_theta0
         theta1 -= tmp_theta1
-    theta[0] = theta0
-    theta[1] = theta1
-    return theta
+    theta0 = theta0*(max(y)-min(y)) + min(y) + (theta1*min(x)*(min(y)-max(y)))/(max(x)-min(x))
+    theta1 = theta1*(max(y)-min(y)) / (max(x)-min(x))
+    return theta0, theta1
 
 if __name__ == "__main__":
     df = pd.read_csv("data.csv")
@@ -67,10 +54,11 @@ if __name__ == "__main__":
         print(f"Error: \"{filename}\" file not found or wrong format.")
         exit(0)
     print(km.shape)
-    y = predict(theta, km)
+    yp = predict(theta, km)
     print(km)
-    print(y)
+    print(yp)
+    
     lr = 0.2 #learning rate = alpha
-    theta = fit(theta, km, y, lr, 1)
+    theta = linear_regression(km, price, km.size, theta[0], theta[1], 0.1, 10000)
     print(theta)
-    #WIP
+
